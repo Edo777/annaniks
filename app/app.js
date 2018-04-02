c92e19f315b5991fd9a157a97e2e5dcf21291c14
Vano_Varderesyan <vano.varderesyan@mail.ru>
2018-04-02 12:22:00 +0400

    fixed swagger bugs

--- app/app.js
+++ app/app.js
@@ -15,28 +15,28 @@ const expressOasGenerator = require('express-oas-generator');
 class API {
     initApp(app) {
 
-        const expressSwagger = require('express-swagger-generator')(app);
+        // const expressSwagger = require('express-swagger-generator')(app);
 
-        let options = {
-            swaggerDefinition: {
-                info: {
-                    description: 'This is a sample server',
-                    title: 'Swagger',
-                    version: '1.0.0',
-                },
-                host: 'localhost:3000',
-                basePath: '',
-                produces: [
-                    "application/json",
-                    "application/xml"
-                ],
-                schemes: ['http', 'https']
-            },
-            basedir: __dirname, //app absolute path
-            files: ['./routers/**/*.js'] //Path to the API handle folder
-        };
-        expressSwagger(options)
-        //configs
+        // let options = {
+        //     swaggerDefinition: {
+        //         info: {
+        //             description: 'This is a sample server',
+        //             title: 'Swagger',
+        //             version: '1.0.0',
+        //         },
+        //         host: 'localhost:3000',
+        //         basePath: '',
+        //         produces: [
+        //             "application/json",
+        //             "application/xml"
+        //         ],
+        //         schemes: ['http', 'https']
+        //     },
+        //     basedir: __dirname, //app absolute path
+        //     files: ['./routers/**/*.js'] //Path to the API handle folder
+        // };
+        // expressSwagger(options)
+        // //configs
         
         app.use(function (req, res, next) {
 
